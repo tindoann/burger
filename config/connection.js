8ddf3b95567e77cb942d2aedda6ceae93cbbda7b
tindoann <tindoann@gamil.com>
2019-09-19 22:46:24 -0400

    added JAWSDB to connections

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 
 // Connect Node to MySQL 
 const mysql = require("mysql");
+const connection; 
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 8080,
-  user: "root",
-  password: "Huong1225",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection({process.env.JAWSDB_URL); 
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 8080,
+    user: "root",
+    password: "Huong1225",
+    database: "burgers_db"
+  });
+}; 
 
 connection.connect(function(err) {
   if (err) {
